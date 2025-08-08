cf38b18275a4c8aebd278ffc32e05654becc4ecf
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-08-08 17:22:27 +0100

    INITIALISATION MIGRATION 2

--- database/migrations/2025_08_08_000001_create_users_table.php
+++ database/migrations/2025_08_08_000001_create_users_table.php
@@ -12,11 +12,27 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('role_id')->nullable();
+            $table->integer('statut_id')->nullable();
+
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->index();
             $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
+
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
+
             $table->rememberToken();
+
+            $table->unsignedBigInteger('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->integer('first_use')->default(1);
+
             $table->timestamps();
             $table->softDeletes();
         });
@@ -26,7 +42,10 @@ return new class extends Migration
             'name' => 'Administrateur',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
+            'statut_id' => 1,
+            'role_id' => 1,
             'email_verified_at' => now(),
+            'first_use' => 0,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
