ba6d9ba68c031dc901f0e2698a88f32742fec117
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-08-10 13:05:50 +0100

    Correction fiche

--- resources/views/livewire/admin/agents/fiche.blade.php
+++ resources/views/livewire/admin/agents/fiche.blade.php
@@ -130,7 +130,12 @@
                                                 <h6>{{ $inactifAgent?->prenom . ' ' . $inactifAgent?->nom }}</h6>
                                                 <p>{{ $inactifAgent?->poste?->titre }}</p>
                                             </div>
-                                            @if (Auth::user()->agent->id == $inactifAgent?->id)
+                                            @php
+                                                $currentUserId = Auth::id();
+                                                $isCurrentUser = $currentUserId === 1 || 
+                                                              (Auth::user()->agent && Auth::user()->agent->id === $inactifAgent?->id);
+                                            @endphp
+                                            @if ($isCurrentUser)
                                                 <small class="badge bg-info ms-3" style="font-size:8px">Vous</small>
                                             @endif
                                         </div>
