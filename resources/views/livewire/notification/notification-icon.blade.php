d53ee6a9fdd09fd9890284b5c9434d5c05665194
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-08-13 11:00:51 +0100

    Correction Notification et boite de reception

--- resources/views/livewire/notification/notification-icon.blade.php
+++ resources/views/livewire/notification/notification-icon.blade.php
@@ -1,10 +1,12 @@
-<a href="" class="link me-4" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNotif"
-    aria-controls="offcanvasRight" wire:poll="fetchNotif">
-    <div class="tooltip-indicator">
-        Notifications
-    </div>
-    <i class="fi fi-rr-bell"></i>
-    @if ($notifications->count())
-        <span class="blink"></span>
-    @endif
-</a>
+@if (!$isSecretaireDG)
+    <a href="" class="link me-4" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNotif"
+        aria-controls="offcanvasRight" wire:poll="fetchNotif">
+        <div class="tooltip-indicator">
+            Notifications
+        </div>
+        <i class="fi fi-rr-bell"></i>
+        @if ($notifications->count())
+            <span class="blink"></span>
+        @endif
+    </a>
+@endif
