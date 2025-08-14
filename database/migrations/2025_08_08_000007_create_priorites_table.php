713194062c7043bfe0805da897d689ef08ab8a7c
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-08-14 14:42:02 +0100

    Correction boutons show task

--- database/migrations/2025_08_08_000007_create_priorites_table.php
+++ database/migrations/2025_08_08_000007_create_priorites_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     public function up()
@@ -14,6 +14,13 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        DB::table('priorites')->insert([
+            ['titre' => 'Faible', 'created_at' => now(), 'updated_at' => now()],
+            ['titre' => 'Moyenne', 'created_at' => now(), 'updated_at' => now()],
+            ['titre' => 'Forte', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+        
     }
 
     public function down()
--- database/migrations/2025_08_08_000009_create_lieu_affectations_table.php
+++ database/migrations/2025_08_08_000009_create_lieu_affectations_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,6 +15,12 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        DB::table('lieu_affectations')->insert([
+            ['titre' => 'Kinshasa-Limete', 'created_at' => now(), 'updated_at' => now()],
+            ['titre' => 'Kinshasa-Maluku', 'created_at' => now(), 'updated_at' => now()],
+            ['titre' => 'Matadi', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     public function down()
