8b718255bcf59e431af9b5166345a60addf1ba60
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-08-20 13:56:14 +0100

    correction Notifications taches 2

--- app/Listeners/SendTacheCreatedNotification.php
+++ app/Listeners/SendTacheCreatedNotification.php
@@ -30,6 +30,16 @@ class SendTacheCreatedNotification
     public function handle(TacheCreated $event)
     {
         $agents = Agent::find($event->agents);
-        Notification::send($agents, new TacheNotification($event->tache, $event->message));
+        $agentQuiADemarre = auth()->user()->agent;
+        
+        // Si le message contient "est en cours de traitement", on l'ajoute au début
+        if (str_contains($event->message, 'est en cours de traitement')) {
+            $message = "L'agent " . $agentQuiADemarre->prenom . ' ' . $agentQuiADemarre->nom . " a démarré le traitement de la tâche : \"" . $event->tache->titre . "\"";
+        } else {
+            // Ajouter le nom de la tâche au message existant
+            $message = $event->message . " : \"" . $event->tache->titre . "\"";
+        }
+        
+        Notification::send($agents, new TacheNotification($event->tache, $message, $agentQuiADemarre));
     }
 }
