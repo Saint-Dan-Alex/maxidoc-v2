32af4c16159856fe9512913556e95414dda76167
Saint_Dan_Alex <dandannykabuya@gmail.com>
2025-10-21 17:52:03 +0100

    Correction show courrier

--- resources/views/regidoc/pages/courriers/show-courrier.blade.php
+++ resources/views/regidoc/pages/courriers/show-courrier.blade.php
@@ -2559,22 +2559,23 @@
                             location.reload();
                         }, 1000);
                     },
-                    // error: function(xhr) {
-                    //     console.error('Erreur lors de la requête:', xhr);
-                    //     let errorMessage = 'Une erreur est survenue lors de la communication avec le serveur';
+                    // Saint Dan Alex
+                    error: function(xhr) {
+                        console.error('Erreur lors de la requête:', xhr);
+                        let errorMessage = 'Une erreur est survenue lors de la communication avec le serveur';
                         
                         
-                    //     if (xhr.status === 422 && xhr.responseJSON && xhr.responseJSON.errors) {
-                    //         errorMessage = Object.values(xhr.responseJSON.errors).flat().join('\n');
-                    //     } else if (xhr.responseJSON && xhr.responseJSON.message) {
-                    //         errorMessage = xhr.responseJSON.message;
-                    //     }
+                        if (xhr.status === 422 && xhr.responseJSON && xhr.responseJSON.errors) {
+                            errorMessage = Object.values(xhr.responseJSON.errors).flat().join('\n');
+                        } else if (xhr.responseJSON && xhr.responseJSON.message) {
+                            errorMessage = xhr.responseJSON.message;
+                        }
                         
-                    //     Livewire.emit('alert', 'error', errorMessage);
-                    //     $('page-load').addClass('d-none');
-                    //     submitBtn.disabled = false;
-                    //     submitBtn.innerHTML = originalBtnText;
-                    // }
+                        Livewire.emit('alert', 'error', errorMessage);
+                        $('page-load').addClass('d-none');
+                        submitBtn.disabled = false;
+                        submitBtn.innerHTML = originalBtnText;
+                    }
                 });
             });
 
